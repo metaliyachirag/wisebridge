65f8ed0b13f9f252997fde9e56b25054a5a7ef73
dhruvpatel-uwin <136035551+dhruvpatel-uwin@users.noreply.github.com>
2023-07-18 18:24:46 -0400

    WU-25 added FirebaseDatabase for Reviewpage

--- Reviewpage .java
+++ Reviewpage .java
@@ -1,15 +1,24 @@
 package com.example.wisebridge;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +26,7 @@ import java.util.List;
 public class Reviewpage extends AppCompatActivity {
     public static String RKeys,RDesc,Rido,types;
     TextView Cnamee,Cdesc,Cowner;
+    DatabaseReference databaseReference;
     EditText revdes,revrate;
     Button revbut;
     public static RecyclerView ReviewsRecyclerView;
@@ -40,6 +50,39 @@ public class Reviewpage extends AppCompatActivity {
         reviewsAdaptors = new ReviewsAdaptor(Reviewsz);
         ReviewsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        FirebaseDatabase ref1 = FirebaseDatabase.getInstance("https://wisebridge-c303a-default-rtdb.firebaseio.com/");
+        databaseReference = ref1.getReference().child("Content").child(RKeys).child("reviews");
+        ReviewsRecyclerView.setAdapter(reviewsAdaptors);
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NotNull DataSnapshot dataSnapshot) {
+                Reviewsz.clear();
+                reviewsAdaptors.notifyDataSetChanged();
+                if(dataSnapshot.exists()){
+                    for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                        if(childSnapshot.exists()){
+                            String owner = childSnapshot.getKey(); // Get the child root value (ID)
+                            String rev = childSnapshot.child("rev").getValue(String.class); // Get the name
+                            String rate = childSnapshot.child("rate").getValue().toString();
+                            Reviews review = new Reviews(owner, rev, rate); // Create a new Post object
+                            Reviewsz.add(review);
+                            //Toast.makeText(Reviewpage.this, rev, Toast.LENGTH_SHORT).show();
+                        }
+                        reviewsAdaptors.notifyDataSetChanged();
+                    }
+                }
+
+
+                else{
+                    Toast.makeText(Reviewpage.this, "No reviews yet", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NotNull DatabaseError databaseError) { }
+        });
 
         //Toast.makeText(Reviewpage.this, "You pressed: "+RKeys +"\nwith description: " + RDesc + "\nwith ID: "+ Rido, Toast.LENGTH_SHORT).show();
         //Log.d("Tag","You pressed: "+RKeys +"\nwith description: " + RDesc + "\nwith ID: "+ Rido);
